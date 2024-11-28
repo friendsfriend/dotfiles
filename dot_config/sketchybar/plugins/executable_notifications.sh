94959523316f5b5dd2d9031e3766cd914f2ccbfd
Fabian Kellner <F19918@eon.com>
2024-11-28 16:19:43 +0100

    add calender with meetings

--- dot_config/sketchybar/plugins/executable_notifications.sh
+++ dot_config/sketchybar/plugins/executable_notifications.sh
@@ -8,60 +8,51 @@ NOTIFICATION_STATUS_COLOR=$TRANSPARENT
 
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Microsoft Outlook")
 ICON=":mail:"
-drawing="off"
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     LABEL="${BASH_REMATCH[1]}"
 
     if [[ $LABEL == "•" ]]; then
           NOTIFICATION_STATUS_COLOR=$TRANSPARENT_YELLOW
-          drawing="on"
     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
         NOTIFICATION_STATUS_COLOR=$TRANSPARENT_RED
-        drawing="on"
     fi
 fi
 
-sketchybar --set outlook icon=$ICON label="${LABEL}" drawing=$drawing
+sketchybar --set outlook icon=$ICON label="${LABEL}" 
 
 
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Microsoft Teams")
 ICON=":microsoft_teams:"
-drawing="off"
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     LABEL="${BASH_REMATCH[1]}"
 
    if [[ $LABEL == "•" ]]; then
         if [[ $NOTIFICATION_STATUS_COLOR != $TRANSPARENT_RED ]]; then
           NOTIFICATION_STATUS_COLOR=$TRANSPARENT_YELLOW
-          drawing="on"
         fi
     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
         NOTIFICATION_STATUS_COLOR=$TRANSPARENT_RED
-        drawing="on"
     fi
 fi
 
-sketchybar --set teams icon=$ICON label="${LABEL}" drawing=$drawing
+sketchybar --set teams icon=$ICON label="${LABEL}" 
 
 
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Mattermost")
 ICON=":mattermost:"
-drawing="off"
 if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
     LABEL="${BASH_REMATCH[1]}"
 
     if [[ $LABEL == "•" ]]; then
         if [[ $NOTIFICATION_STATUS_COLOR != $TRANSPARENT_RED ]]; then
           NOTIFICATION_STATUS_COLOR=$TRANSPARENT_YELLOW
-          drawing="on"
         fi
     elif [[ $LABEL =~ ^[0-9]+$ ]]; then
         NOTIFICATION_STATUS_COLOR=$TRANSPARENT_RED
-        drawing="on"
     fi
 fi
 
-sketchybar --set mattermost icon=$ICON label="${LABEL}" drawing=$drawing 
+sketchybar --set mattermost icon=$ICON label="${LABEL}" 
 
 sketchybar --bar color=$NOTIFICATION_STATUS_COLOR  \
                  height=37        \

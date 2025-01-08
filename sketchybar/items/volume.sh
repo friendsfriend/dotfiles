7788a9497d7c388ce2c4949bfe56a6be367ee489
Fabian Kellner <F19918@eon.com>
2025-01-08 12:56:35 +0100

    update sketchyba

--- sketchybar/items/volume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item volume right \
-           --set volume script="$PLUGIN_DIR/volume.sh" icon.font="sketchybar-app-font:Regular:16.0" \
-                 background.border_width=0 \
-           --subscribe volume volume_change \

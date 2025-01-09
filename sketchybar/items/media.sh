888c5ddb1034e6ba87edf7751a934636afb52a34
Fabian Kellner <F19918@eon.com>
2025-01-09 12:57:39 +0100

    remove media plugin from sketchybar

--- sketchybar/items/media.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item media left \
-           --set media label.max_chars=20 \
-                       scroll_texts=on \
-                       icon=􀑪             \
-                       script="$PLUGIN_DIR/media.sh" \
-           --subscribe media media_change

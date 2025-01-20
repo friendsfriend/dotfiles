6980afac3a276dd09d65587dcf96da1221fd9dc2
Fabian Kellner <F19918@eon.com>
2025-01-20 11:08:24 +0100

    add zscaler toggle

--- sketchybar/plugins/zscaler.sh
+++ sketchybar/plugins/zscaler.sh
@@ -2,10 +2,12 @@
 
 source "$CONFIG_DIR/colors.sh" # Loads all defined colors
 
-if pgrep -i zscaler > /dev/null; then
+if sudo launchctl list | grep zscaler > /dev/null; then
     sketchybar --set zscaler background.color=$TRANSPARENT_PURPLE \
+               --set zscaler click_script="sudo /opt/scripts/zscaler-kill.sh stop" \
                --set zscaler label="on"
 else
     sketchybar --set zscaler background.color=$TRANSPARENT \
+               --set zscaler click_script="sudo /opt/scripts/zscaler-kill.sh start" \
                --set zscaler label="off"
 fi

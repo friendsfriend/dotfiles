2862d510a114a361615c290ebe260f0432ebad5b
Fabian Kellner <F19918@eon.com>
2025-01-27 13:43:25 +0100

    fix install script

--- sketchybar/resources/setup-zscaler.sh
+++ sketchybar/resources/setup-zscaler.sh
@@ -5,4 +5,4 @@ cp zscaler /etc/sudoers.d/zscaler
 chmod 0600 /etc/sudoers.d/zscaler
 mkdir -p /opt/scripts
 cp zscaler-kill.sh /opt/scripts/zscaler-kill.sh
-chmod o=rwx,g=,o= /opt/scripts/zscaler-kill.sh
+chmod u=rwx,g=,o= /opt/scripts/zscaler-kill.sh

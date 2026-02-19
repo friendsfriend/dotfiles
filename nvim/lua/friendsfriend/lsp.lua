b9323fcbe116967e8dec9e6eca5ff55ae3310012
Fabian Kellner <F19918@eon.com>
2026-02-19 11:16:03 +0100

    improve hover behavior for lsp

--- nvim/lua/friendsfriend/lsp.lua
+++ nvim/lua/friendsfriend/lsp.lua
@@ -5,16 +5,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- Buffer local mappings.
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
 		local opts = { buffer = ev.buf, silent = true }
-
 		-- set keybinds
 		opts.desc = "See available code actions"
 		keymap.set("n", "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
+		opts.desc = "Lsp hover"
+		keymap.set("n", "K", function()
+			vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
+		end, opts)
+
+		opts.desc = "Diagnostic hover"
+		keymap.set("n", "H", function()
+			vim.diagnostic.open_float({ border = "single", max_height = 25, max_width = 120 })
+		end, opts)
+
 		opts.desc = "Smart rename"
 		keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts) -- smart rename
-
-		opts.desc = "Show line diagnostics"
-		keymap.set("n", "<leader>lE", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 		opts.desc = "Go to previous diagnostic"
 		keymap.set("n", "<leader>lep", function()
@@ -25,9 +31,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		keymap.set("n", "<leader>len", function()
 			vim.diagnostic.jump({ count = 1, float = true })
 		end, opts) -- jump to next diagnostic in buffer
-
-		opts.desc = "Show documentation for what is under cursor"
-		keymap.set("n", "<leader>lh", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 	end,
 })
 
@@ -44,4 +47,6 @@ vim.diagnostic.config({
 			[severity.INFO] = " ",
 		},
 	},
+	virtual_text = { current_line = true },
+	-- virtual_lines = { current_line = true },
 })

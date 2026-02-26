52fe32c75c1241db1af8aba283e3c40c27b0dc92
Fabian Kellner <F19918@eon.com>
2026-02-26 08:51:45 +0100

    small fix

--- nvim/lua/friendsfriend/plugins/typst.lua
+++ nvim/lua/friendsfriend/plugins/typst.lua
@@ -2,7 +2,6 @@ return {
 	"chomosuke/typst-preview.nvim",
 	ft = "typst",
 	version = "1.*",
-	opts = {}, -- lazy.nvim will implicitly calls `setup {}`
 	config = function()
 		vim.keymap.set({ "n", "<leader>tp", "<cmd>TypstPreview<cr>", { desc = "Start typst live preview" } })
 	end,

c5514d369cfd87f840f28e30da86118ca4aa8fac
Fabian Kellner <fabiankellner@mac-83c3eb6a.fritz.box>
2026-03-26 10:53:58 +0100

    fix packages config

--- sketchybar/items/packages.sh
+++ sketchybar/items/packages.sh
@@ -3,6 +3,6 @@
 sketchybar -m --add item packages right \
               --set packages update_freq=120 \
                              script="$PLUGIN_DIR/packages.sh" \
-                             click_script="sketchybar --set packages label=''; brew upgrade ; ./$PLUGIN_DIR/packages.sh" \
+                             click_script="bash $PLUGIN_DIR/packages.sh --upgrade" \
                              icon=""                         \
-                             label=""
+                             label="✓"
